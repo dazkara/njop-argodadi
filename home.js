dc2fda5166ebbe193d34ccefa344ce44ad908dff
dazkara <73165473+dazkara@users.noreply.github.com>
2024-04-16 01:43:58 +0700

    home.js

--- home.js
+++ home.js
@@ -286,5 +286,5 @@ function createLegend(colorScale, title, attribute) {
 
 
 function numberWithCommas(x) {
-    return 'Rp.' + x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    return 'Rp' + x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
